b8bf161777e4d2f069b99b4e58557fd7ec7c8c67
kannaq <zagubionyt@gmail.com>
2016-03-11 20:46:22 +0100

    First change

---
+++ inc/head.php
@@ -0,0 +1,4 @@
+<title>Charity</title>
+<meta charset="utf-8">
+<meta name="author" content="Kacper Malina">
+<link rel="stylesheet" href="css/master.css" charset="utf-8">
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'inc/head.php'; ?>
+  </head>
+  <body>
+      <?php include 'inc/menu.html';?>
+      <div class="container">
+          <div class="section" role="title">
+            <h1>Hello</h1>
+          </div>
+          <div class="section center" role="progressbar">
+            <h2>90/100</h2>
+            <div class="progress-bar" id="progress">
+              <div class="inner">
+              </div>
+            </div>
+          </div>
+          <div class="section" role="specificinformation">
+            efc
+          </div>
+      </div>
+  </body>
+</html>
